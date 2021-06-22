fcc957580fd48d44b787262fb18356a80a8e4253
Álvaro Patón Nieto <alvaro.paton@uclm.es>
2021-06-22 13:18:22 +0200

    subimos install_repo.sh

---
+++ install_repo.sh
@@ -0,0 +1,1 @@
+ainsible -i inventory -m yum_repository -a "name='EPEL-8' description='epel yum repo' baseurl='https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rmp' enabled=no prod"
